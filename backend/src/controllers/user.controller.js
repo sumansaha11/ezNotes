f2df2fca0e23cd31a6a5a4a5c9b9763d9fdad193
Suman Saha <sumanrider7@gmail.com>
2024-11-28 01:38:02 +0530

    cookie options update

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -5,10 +5,9 @@ import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.model.js";
 
 const options = {
-    sameSite: 'None',
+    sameSite: 'Strict',
     path: '/',
     secure: true,
-    httpOnly: true,
 };
 
 const generateAccessAndRefreshTokens = async (userId) => {
