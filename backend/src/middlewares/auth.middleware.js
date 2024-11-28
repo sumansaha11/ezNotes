6bfd6708a5d4d4c9b4bd0902209fcdf50bbbb512
Suman Saha <sumanrider7@gmail.com>
2024-11-29 01:04:40 +0530

    auth update

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -1,17 +1,17 @@
-import jwt from "jsonwebtoken";
+import JWT from "jsonwebtoken";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.model.js";
 
 const verifyJWT = asyncHandler(async (req, res, next) => {
     try {
-        const token = req.header("Authorization")?.replace("Bearer ", "") || req.cookies?.accessToken;
+        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
 
         if (!token) {
             throw new ApiError(401, "Unauthorized request! Token missing.");
         }
 
-        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+        const decodedToken = JWT.verify(token, process.env.ACCESS_TOKEN_SECRET);
 
         if (!decodedToken || !decodedToken._id) {
             throw new ApiError(401, "Invalid token payload.");
@@ -33,42 +33,3 @@ const verifyJWT = asyncHandler(async (req, res, next) => {
 });
 
 export { verifyJWT };
-
-
-
-
-
-
-
-
-// import jwt from "jsonwebtoken";
-// import { asyncHandler } from "../utils/asyncHandler.js";
-// import { ApiError } from "../utils/ApiError.js";
-// import { User } from "../models/user.model.js";
-
-// const verifyJWT = asyncHandler( async (req, res, next) => {
-//     try {
-//         const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
-        
-//         if (!token) {
-//             throw new ApiError(401, "Unauthorized request!");
-//         }
-    
-//         const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-        
-//         const user = await User.findById(decodedToken?._id).select(
-//             "-password -refreshToken"
-//         )
-//         if (!user) {
-//             throw new ApiError(401, "Invalid Access token!");
-//         }
-        
-//         req.user = user;
-//         next();
-
-//     } catch (error) {
-//         throw new ApiError(401, error?.message || "INVALID ACCESS TOKEN!");
-//     }
-// });
-
-// export { verifyJWT };
