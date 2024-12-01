dea3a80959e4814527171f7a553610e1c7f77d65
Suman Saha <sumanrider7@gmail.com>
2024-12-01 23:55:16 +0530

    build edit

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,13 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import path from "node:path";
+import { fileURLToPath } from "url";
 
 const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
@@ -21,5 +26,10 @@ import noteRouter from "./routes/note.routes.js";
 //routes declaration
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/notes", noteRouter);
+
+app.use(express.static(path.join(__dirname, "./dist")));
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./dist/index.html"));
+});
 
 export { app };
