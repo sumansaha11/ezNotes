fab7b16276db49dd962c9d61b6658414e0eefee1
Suman Saha <sumanrider7@gmail.com>
2024-12-02 14:56:06 +0530

    axios update

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -6,31 +6,3 @@ axiosInstance.defaults.baseURL = import.meta.env.VITE_BASE_URL;
 axiosInstance.defaults.withCredentials = true;
 
 export default axiosInstance;
-
-// import axios from "axios";
-// import { Cookies } from "react-cookie";
-
-// const cookies = new Cookies();
-
-// const axiosInstance = axios.create({
-//     baseURL: import.meta.env.VITE_BASE_URL,
-//     timeout: 10000,
-//     headers: {
-//         "Content-Type": "application/json",
-//     },
-// });
-
-// axiosInstance.interceptors.request.use(
-//     (config) => {
-//         const accessToken = cookies.get('accessToken');
-//         if (accessToken) {
-//             config.headers.Authorization = `Bearer ${accessToken}`;
-//         }
-//         return config;
-//     },
-//     (error) => {
-//         return Promise.reject(error);
-//     }
-// );
-
-// export default axiosInstance;
